7b5fe9613e7f206c64884db40efa217deceee396
Erica Edwards <ericae14@hotmail.com>
2021-01-16 19:04:14 -0800

    ready to put on heroku

---
+++ unix_time.py
@@ -0,0 +1,14 @@
+import time
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/unix_time/")
+def get_time():
+    return str(time.time(int))
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("Port", "6738"))
+    app.run(host="0.0.0.0", port=port)
