49e4b8dce24e3754a2401d8c66386161a48a0d55
Erica Edwards <ericae14@hotmail.com>
2021-01-22 10:25:17 -0800

    corection

--- unix_time.py
+++ unix_time.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/unix_time/")
 def get_time():
-    return str(time.time(int))
+    return str(time.time())
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "6738"))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="", port=port)
