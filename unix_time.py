f4c62b2201ad375f65689b227b4f62eb1e4641ea
Erica Edwards <ericae14@hotmail.com>
2021-01-22 10:08:19 -0800

    Correction

--- unix_time.py
+++ unix_time.py
@@ -10,5 +10,5 @@ def get_time():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("Port", "6738"))
+    port = int(os.environ.get("PORT", "6738"))
     app.run(host="0.0.0.0", port=port)
